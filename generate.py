43be8444a55c0d914b8c2d34c7a82222a489a088
mstone3201 <60367690+maxwellstone@users.noreply.github.com>
2023-01-09 17:50:22 -0600

    25 Towers

--- generate.py
+++ generate.py
@@ -1,14 +1,19 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 
 x = 0
 y = 0
-z = 1.5
+z = 0.5
 length = 1
-width = 2
-height = 3
+width = 1
+height = 1
 
-pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
+for i in range(5):
+    for j in range(5):
+        scale = 1
+        for k in range(10):
+            pyrosim.Send_Cube(name="Box", pos=[x - 2.5 + i, y - 2.5 + j, z + k], size=[length * scale, width * scale, height * scale])
+            scale *= 0.9
 
 pyrosim.End()
